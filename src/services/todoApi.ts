f928e4cd86a09969c48aaf4b8c14cc6c8273aa26
Ilya Dvorchuk <iladvorchuk@gmail.com>
2024-08-20 22:43:38 +0300

    feat: add saving todo in localstorage

--- src/services/todoApi.ts
+++ src/services/todoApi.ts
@@ -1,35 +1,38 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 import {Todo} from "../store/reducers/Todos/TodosTypes";
 
-let mockTodos: Todo[] = [
-    { id: '1', text: 'Learn React', completed: false },
-    { id: '2', text: 'Learn Redux', completed: true },
-    { id: '3', text: 'Learn TypeScript', completed: false },
-];
+
+const saveToLocalStorage = (todos: Todo[]) => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+};
+
 
 export const todosApi = createApi({
     reducerPath: 'todosApi',
     baseQuery: async ({ method, body, url }) => {
         await new Promise((resolve) => setTimeout(resolve, 1000));
+        let mockTodos: Todo[] = JSON.parse(localStorage.getItem('todos') || '[]');
 
         const urlParts = url?.split('/');
         const id = urlParts?.length > 1 ? urlParts[urlParts.length - 1] : undefined;
-
         switch (method) {
             case 'GET':
                 return { data: mockTodos };
             case 'POST':
                 const newTodo = { ...body, id: Date.now().toString() };
                 mockTodos.push(newTodo);
+                saveToLocalStorage(mockTodos);
                 return { data: newTodo };
             case 'PUT':
                 mockTodos = mockTodos.map(todo =>
                     todo.id === body.id ? body : todo
                 );
+                saveToLocalStorage(mockTodos);
                 return { data: body };
             case 'DELETE':
                 if (id) {
                     mockTodos = mockTodos.filter(todo => todo.id !== id);
+                    saveToLocalStorage(mockTodos);
                     return { data: { success: true, id } };
                 } else {
                     return { error: 'ID not provided for DELETE' };
